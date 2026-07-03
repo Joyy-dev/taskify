7f9bf4106af85830aaa81912aca4b4e604d60f80
joyy_dev <joyceava874@gmail.com>
2026-07-03 13:00:43 +0100

    update on authentication backend

--- backend/src/services/auth.service.js
+++ backend/src/services/auth.service.js
@@ -1,4 +1,5 @@
 const userRepository = require("../repositories/user.repository");
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt')
 
 class AuthService {
@@ -17,7 +18,54 @@ class AuthService {
             password: hashedPassword
         };
 
-        return userRepository.create(newUser);
+        const createdUser = await userRepository.create(newUser);
+        
+        const userResponse = {
+            id: createdUser.id,
+            name: createdUser.name,
+            email: createdUser.email
+        };
+
+        return userResponse;
+    }
+
+    async login({ email, password }) {
+        const user = await userRepository.findByEmail(email);
+
+        if (!user) {
+            throw new Error('Invalid credentials');
+        }
+
+        const isMatch = await bcrypt.compare(
+            password, 
+            user.password
+    );
+
+        if (!isMatch) {
+            throw new Error('Invalid credentials');
+
+        };
+    
+        const payload = {
+            id: user.id
+        };
+        
+        const token = jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            {expiresIn: process.env.JWT_EXPRIES_IN}
+        );
+
+        const userResponse = {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+        };
+
+        return {
+            user: userResponse,
+            token
+        };
     }
 }
 
