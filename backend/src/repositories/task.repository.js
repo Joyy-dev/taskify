c2c552f10233f5ea1be12f42241044b24e0ae3cf
joyy_dev <joyceava874@gmail.com>
2026-08-24 12:25:25 +0100

    changing in-memory to PostgrSQL for task repository

--- backend/src/repositories/task.repository.js
+++ backend/src/repositories/task.repository.js
@@ -1,45 +1,92 @@
-const tasks = [];
+const pool = require("../config/database");
 
 class TaskRepository {
     async findById(id) {
-        return tasks.find(task => task.id === id) ?? null;
+        const result = await pool.query(
+            `
+            SELECT *
+            FROM tasks
+            WHERE id = $1
+            `,
+            [id]
+        )
+        return result.rows[0] ?? null;
     }
 
     async findByUserId(userId) {
-        return tasks.filter(task => task.userId === userId);
+        const result = await pool.query(
+            `
+            SELECT *
+            FROM tasks
+            WHERE user_id = $1
+            `,
+            [userId]
+        )
+        return result.rows
     }
 
     async create(task) {
-        const newTask = {
-            id: tasks.length + 1,
-            ...task 
-        };
-        tasks.push(newTask);
-        return newTask;
+        const newTask = await pool.query(
+            `
+            INSERT INTO tasks(user_id, title, description, priority, due_date, completed)
+            VALUES($1, $2, $3, $4, $5, $6)
+            RETURNING *;
+            `,
+            [task.userId, task.title, task.description, task.priority, task.dueDate, task.completed]
+        )
+        return newTask.rows[0];
     }
 
     async update(id, updates) {
-        const taskIndex = tasks.findIndex(task => task.id === id);
-
-        if (taskIndex === -1) {
-            return null;
+        const allowedFields = {
+            title: 'title',
+            description: 'description',
+            priority: 'priority',
+            dueDate: 'due_date',
+            completed: 'completed'
         }
 
-        tasks[taskIndex] = {
-            ...tasks[taskIndex], 
-            ...updates 
-        };
-        return tasks[taskIndex];
+        const fields = Object.keys(updates).filter(
+            (key) => allowedFields[key]
+        );
+        
+        if (fields.length === 0) {
+            return null;
+        }
+        
+        const setClause = Object.keys(updates).map(
+            (key, index) => `${key} = $${index + 1}`
+        );
+
+        const values = Object.values(updates);
+        
+
+        values.push(id);
+
+        const result = await pool.query(
+            `
+            UPDATE tasks
+            SET ${setClause.join(', ')}
+            WHERE id = $${values.length}
+            RETURNING *;
+            `,
+            values
+        )
+
+        return result.rows[0] ?? null;
     }
 
     async delete(id) {
-        const taskIndex = tasks.findIndex(task => task.id === id);
+        const result = await pool.query(
+            `
+            DELETE FROM tasks
+            WHERE id = $1
+            RETURNING *;
+            `,
+            [id]
+        )
 
-        if (taskIndex === -1) {
-            return null;
-        }
-
-        return tasks.splice(taskIndex, 1)[0];
+        return result.rows[0] ?? null;
     }
 }
 
